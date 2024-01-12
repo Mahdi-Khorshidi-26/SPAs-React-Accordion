54bd9b522b65f636a8282d1df931fe31ead976d2
Mahdi-Khorshidi-26 <khorshidi.mahdi.1998@gmail.com>
2024-01-12 21:28:11 +0330

    uploading the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const dataQ = [
+  {
+    title: "Are you there ?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt hic dicta aperiam, totam optio. Quasi temporibus vero ea pariatur, nulla nesciunt, molestias, ducimus quidem amet nemo mollitia quos earum!",
+  },
+  {
+    title: "Are you there ?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt hic dicta aperiam, totam optio. Quasi temporibus vero ea pariatur, nulla nesciunt, molestias, ducimus quidem amet nemo mollitia quos earum!",
+  },
+  {
+    title: "Are you there ?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt hic dicta aperiam, totam optio. Quasi temporibus vero ea pariatur, nulla nesciunt, molestias, ducimus quidem amet nemo mollitia quos earum!",
+  },
+  {
+    title: "Are you there ?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident incidunt hic dicta aperiam, totam optio. Quasi temporibus vero ea pariatur, nulla nesciunt, molestias, ducimus quidem amet nemo mollitia quos earum!",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={dataQ} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  const [currOpen, setCurrOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((item, i) => (
+        <AccordionItem
+          title={item.title}
+          num={i}
+          onOpen={setCurrOpen}
+          currOpen={currOpen}
+          key={i}
+        >
+          {item.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ children, title, num, onOpen, currOpen }) {
+  let isOpen = num === currOpen;
+
+  function toggleOpen() {
+    // setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""} animated-opener`}
+      onClick={() => toggleOpen()}
+    >
+      <p className="number">{num < 9 ? `0${num + 1}` : `${num + 1}`}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box animated-opener">{children}</div>}
+    </div>
+  );
+}
